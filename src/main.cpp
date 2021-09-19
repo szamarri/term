a87bf3e088a4063c7e6c49cc844903ca64b71811
Saul Zamarripa Rivera <saul.zamarripa.rivera@gmail.com>
2021-09-19 15:16:18 -0500

    add std::vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 #include"item.h"
 
 int main(int argc, char **argv)
 {
-	Item* item = new Item(1, "Saul");
-	std::cout << "Hola mundo!!!" << std::endl;
-	std::cout << "item " << item->getId() << " nombre " << item->getName() << std::endl;
+	int integrantes;
+
+	std::vector<Item *> list;
+	std::cout << "numero de integrantes: ";
+	std::cin >> integrantes;
+
+	for (std::size_t i = 0; i < integrantes; i++)
+	{
+		std::string name;
+		std::cout << "introduce el item " << i+1 << " ";
+		std::cin >> name;
+
+		Item* item = new Item(i+1, name);
+		list.push_back(item);
+	}
+	
+	std::cout << "\n> Los elementos de la lista son: " << std::endl;
+	for (auto item : list)
+	{
+		std::cout << "id: " << item->getId();
+		std::cout << " name: " << item->getName() << std::endl;
+	}
+	
 	return 0;
 }
 
